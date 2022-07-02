21eecf149a2e0af3d30089b54a3678a1dbf06938
ialej001 <ivan.alejandre@gmail.com>
2022-07-02 12:03:55 -0700

    added moves history to game

--- src/index.js
+++ src/index.js
@@ -79,6 +79,16 @@ class Game extends React.Component {
     const winner = calculateWinner(current.squares);
     let status;
 
+    const moves = history.map((step, move) => {
+      const desc = move ? "Go to move #" + move : "Go to game start";
+
+      return (
+        <li>
+          <button onClick={() => this.jumpTo(move)}>{desc}</button>
+        </li>
+      );
+    });
+
     if (winner) {
       status = "Winner: " + winner;
     } else {
@@ -92,7 +102,7 @@ class Game extends React.Component {
         </div>
         <div className="game-info">
           <div>{status}</div>
-          <ol>{/* TODO */}</ol>
+          <ol>{moves}</ol>
         </div>
       </div>
     );
